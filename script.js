89a53edfb98ef32a3ccff4a3e6dae829d9d667cf
Connor Grieves <quickb3n@yahoo.co.uk>
2022-02-08 18:16:54 +0000

    added basic UI and event listener functionality to RPS buttons

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 let playerScore = 0,
     computerScore = 0,
     gamePlaying,
-    i = 0;    
+    i = 0,
+    results = document.querySelector('.displaySection')    
 
 // Generate computer turn
 computerPlay = () => {
@@ -14,10 +15,8 @@ computerPlay = () => {
 
 // Function to play a single round and compare selections to determine a winner for the round
 playRound = (playerSelection, computerSelection) => {
-
-    playerSelection = prompt("Choose between Rock, Paper or Scissors").toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
     computerSelection = computerPlay().toLowerCase();
-
     if (playerSelection === computerSelection) {
         console.log(`Computer plays: ${computerSelection}`);
         console.log(`It's a tie ! The current score is: Player: ${playerScore} computer: ${computerScore}`)
@@ -54,22 +53,46 @@ playRound = (playerSelection, computerSelection) => {
     };    
 };
 
-// Function to play 5 rounds, add up scores and display the winner at the end.
-game = () => {
-    while (i < 5) {
-        i++
-        playRound();
-    };
+// Function to play 5 rounds, add up scores and display the winner at the end. 
+// ---- Commenting out for now as this functionality is not needed pending changes to be made adding in a UI ----
+// game = () => {
+//     while (i < 5) {
+//         i++
+//         playRound();
+//     };
 
-    scores = `Player = ${playerScore}, Computer = ${computerScore}.`
+//     scores = `Player = ${playerScore}, Computer = ${computerScore}.`
 
-    if (playerScore == computerScore) {
-        alert(`GAME OVER. It was a tie, the ending score was ${scores}`)
-    } else if (playerScore > computerScore) {
-        alert(`GAME OVER. You are the winner ! The ending score was ${scores}`)
-    } else if (computerScore > playerScore) {
-        alert(`GAME OVER. You lost ! The ending score was ${scores}`)
-    }
-}
+//     if (playerScore == computerScore) {
+//         alert(`GAME OVER. It was a tie, the ending score was ${scores}`)
+//     } else if (playerScore > computerScore) {
+//         alert(`GAME OVER. You are the winner ! The ending score was ${scores}`)
+//     } else if (computerScore > playerScore) {
+//         alert(`GAME OVER. You lost ! The ending score was ${scores}`)
+//     }
+// }
 
-game();
+// ----DOM Section----
+
+const rock = document.querySelector('.btn_rock')
+const paper = document.querySelector('.btn_paper')
+const scissors = document.querySelector('.btn_scissors')
+
+// Event listeners to toggle the background color of the buttons and reset them after a certain time aswell as run the playRound function
+
+rock.addEventListener('click',() => {
+    playRound('Rock',);
+    rock.classList.toggle('green');
+    setTimeout(() => rock.classList.toggle('green'), 1000)
+});
+paper.addEventListener('click',() => {
+    playRound('Paper',);
+    paper.classList.toggle('green');
+    setTimeout(() => paper.classList.toggle('green'), 1000)
+})
+scissors.addEventListener('click',() => {
+    playRound('Scissors',);
+    scissors.classList.toggle('green');
+    setTimeout(() => scissors.classList.toggle('green'), 1000)
+})
+
