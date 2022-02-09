85fef01d9bc281d539e8ef0632bfd143471559d0
Connor Grieves <quickb3n@yahoo.co.uk>
2022-02-09 15:44:14 +0000

    commit

--- script.js
+++ script.js
@@ -1,59 +1,97 @@
 // Declare global scope variables
 let playerScore = 0,
-    computerScore = 0,
-    gamePlaying,
-    i = 0,
-    results = document.querySelector('.displaySection')    
+  computerScore = 0,
+  gamePlaying,
+  i = 0,
+  results = document.querySelector(".displaySection");
 
 // Generate computer turn
 computerPlay = () => {
-    num = Math.floor(Math.random() * 3);
-    options = ["rock", "paper", "scissors"];
-    computerSelection = options[num];
-    return computerSelection  // Return stores the the value of computerSelection to the caller function. Meaning if we call computerPlay later we get this value.
-}
-
-// Function to play a single round and compare selections to determine a winner for the round
-playRound = (playerSelection, computerSelection) => {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerPlay().toLowerCase();
-    if (playerSelection === computerSelection) {
-        console.log(`Computer plays: ${computerSelection}`);
-        console.log(`It's a tie ! The current score is: Player: ${playerScore} computer: ${computerScore}`)
-    } else if (playerSelection === "rock") {
-        if (computerSelection === "scissors") {
-            playerScore++
-            console.log(`Computer plays: ${computerSelection}`);
-            console.log(`You win ! Rock beats Scissors. The current score is: Player: ${playerScore} computer: ${computerScore}`);
-        } else if (computerSelection === "paper") {
-            computerScore++
-            console.log(`Computer plays: ${computerSelection}`);
-            console.log(`You lose ! Paper Beats Rock. The current score is: Player: ${playerScore} computer: ${computerScore}`);
-        }; 
-    } else if (playerSelection === "scissors") {
-        if (computerSelection === "paper") {
-            playerScore++
-            console.log(`Computer plays: ${computerSelection}`);
-            console.log(`You win ! Scissors beats Paper. The current score is: Player: ${playerScore} computer: ${computerScore}`);
-        } else if (computerSelection === "rock") {
-            computerScore++
-            console.log(`Computer plays: ${computerSelection}`);
-            console.log(`You lose ! Rock beats Scissors. The current score is: Player: ${playerScore} computer: ${computerScore}`);
-        };
-    } else if (playerSelection === "paper") {
-        if (computerSelection === "rock") {
-            playerScore++
-            console.log(`Computer plays: ${computerSelection}`);
-            console.log(`You win ! paper beats Rock. The current score is: Player: ${playerScore} computer: ${computerScore}`);
-        } else if (computerSelection === "scissors") {
-            computerScore++
-            console.log(`Computer plays: ${computerSelection}`);
-            console.log(`You lose ! Scissors beats Paper. The current score is: Player: ${playerScore} computer: ${computerScore}`);
-        };
-    };    
+  num = Math.floor(Math.random() * 3);
+  options = ["rock", "paper", "scissors"];
+  computerSelection = options[num];
+  return computerSelection;
 };
 
-// Function to play 5 rounds, add up scores and display the winner at the end. 
+// Function to play a single round and compare selections to determine a winner for the round
+// Bonus feature: added rules to change the color of the button based on ruleset: If a tie, button goes yellow, if not it goes green for player selection and red for computer selection.
+
+// Next thing that needs worked on: changing console.logs over to DOM methods.
+
+playRound = (playerSelection, computerSelection) => {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerPlay().toLowerCase();
+  if (playerSelection === computerSelection) {
+    choice.classList.toggle("yellow");
+    setTimeout(() => choice.classList.toggle("yellow"), 1000);
+    console.log(`Computer plays: ${computerSelection}`);
+    console.log(
+      `It's a tie ! The current score is: Player: ${playerScore} computer: ${computerScore}`
+    );
+  } else if (playerSelection === "rock") {
+    rock.classList.toggle("green");
+    setTimeout(() => rock.classList.toggle("green"), 1000);
+    if (computerSelection === "scissors") {
+      scissors.classList.toggle("red");
+      setTimeout(() => scissors.classList.toggle("red"), 1000);
+      playerScore++;
+      console.log(`Computer plays: ${computerSelection}`);
+      console.log(
+        `You win ! Rock beats Scissors. The current score is: Player: ${playerScore} computer: ${computerScore}`
+      );
+    } else if (computerSelection === "paper") {
+      paper.classList.toggle("red");
+      setTimeout(() => paper.classList.toggle("red"), 1000);
+      computerScore++;
+      console.log(`Computer plays: ${computerSelection}`);
+      console.log(
+        `You lose ! Paper Beats Rock. The current score is: Player: ${playerScore} computer: ${computerScore}`
+      );
+    }
+  } else if (playerSelection === "scissors") {
+    scissors.classList.toggle("green");
+    setTimeout(() => scissors.classList.toggle("green"), 1000);
+    if (computerSelection === "paper") {
+      paper.classList.toggle("red");
+      setTimeout(() => paper.classList.toggle("red"), 1000);
+      playerScore++;
+      console.log(`Computer plays: ${computerSelection}`);
+      console.log(
+        `You win ! Scissors beats Paper. The current score is: Player: ${playerScore} computer: ${computerScore}`
+      );
+    } else if (computerSelection === "rock") {
+      rock.classList.toggle("red");
+      setTimeout(() => rock.classList.toggle("red"), 1000);
+      computerScore++;
+      console.log(`Computer plays: ${computerSelection}`);
+      console.log(
+        `You lose ! Rock beats Scissors. The current score is: Player: ${playerScore} computer: ${computerScore}`
+      );
+    }
+  } else if (playerSelection === "paper") {
+    paper.classList.toggle("green");
+    setTimeout(() => paper.classList.toggle("green"), 1000);
+    if (computerSelection === "rock") {
+      rock.classList.toggle("red");
+      setTimeout(() => rock.classList.toggle("red"), 1000);
+      playerScore++;
+      console.log(`Computer plays: ${computerSelection}`);
+      console.log(
+        `You win ! paper beats Rock. The current score is: Player: ${playerScore} computer: ${computerScore}`
+      );
+    } else if (computerSelection === "scissors") {
+      scissors.classList.toggle("red");
+      setTimeout(() => scissors.classList.toggle("red"), 1000);
+      computerScore++;
+      console.log(`Computer plays: ${computerSelection}`);
+      console.log(
+        `You lose ! Scissors beats Paper. The current score is: Player: ${playerScore} computer: ${computerScore}`
+      );
+    }
+  }
+};
+
+// Function to play 5 rounds, add up scores and display the winner at the end.
 // ---- Commenting out for now as this functionality is not needed pending changes to be made adding in a UI ----
 // game = () => {
 //     while (i < 5) {
@@ -74,25 +112,21 @@ playRound = (playerSelection, computerSelection) => {
 
 // ----DOM Section----
 
-const rock = document.querySelector('.btn_rock')
-const paper = document.querySelector('.btn_paper')
-const scissors = document.querySelector('.btn_scissors')
+const rock = document.querySelector(".btn_rock");
+const paper = document.querySelector(".btn_paper");
+const scissors = document.querySelector(".btn_scissors");
 
-// Event listeners to toggle the background color of the buttons and reset them after a certain time aswell as run the playRound function
+// Event listeners
 
-rock.addEventListener('click',() => {
-    playRound('Rock',);
-    rock.classList.toggle('green');
-    setTimeout(() => rock.classList.toggle('green'), 1000)
+rock.addEventListener("click", () => {
+  choice = rock;
+  playRound("Rock");
 });
-paper.addEventListener('click',() => {
-    playRound('Paper',);
-    paper.classList.toggle('green');
-    setTimeout(() => paper.classList.toggle('green'), 1000)
-})
-scissors.addEventListener('click',() => {
-    playRound('Scissors',);
-    scissors.classList.toggle('green');
-    setTimeout(() => scissors.classList.toggle('green'), 1000)
-})
-
+paper.addEventListener("click", () => {
+  choice = paper;
+  playRound("Paper");
+});
+scissors.addEventListener("click", () => {
+  choice = scissors;
+  playRound("Scissors");
+});
