abeb7ba3b24b78958a2bdba378d45cc6cee88f4d
Connor Grieves <quickb3n@yahoo.co.uk>
2022-02-11 01:13:52 +0000

    Cleaned up comments and formatted code

--- script.js
+++ script.js
@@ -26,10 +26,7 @@ computerPlay = () => {
   num = Math.floor(Math.random() * 3);
   options = ["rock", "paper", "scissors"];
   computerSelection = options[num];
-  return computerSelection; // Return stores the the value of computerSelection to the caller function. Meaning if we call computerPlay later we get this value.
-};
-
-//Cookies Functions
+  return computerSelection;
 
 //Set Var to Cookie
 function cookieSetVar(varname) {
@@ -41,13 +38,13 @@ function cookieSetVar(varname) {
     return x;
 }
 
-//Update Cookies
+// Update Cookies
 function cookieUpdate(cookieVarName, cookieVarData) {
   document.cookie = cookieVarName + "=" + cookieVarData + ";SameSite=Strict";
   return null;   // The function updates the score cookies
 }
+
 // Function to play a single round and compare selections to determine a winner for the round
-// Bonus feature: add rules to change the color of the button based on ruleset: If a tie, button goes yellow, if not it goes green for player selection and red for computer selection.
 setScore();
 playRound = (playerSelection, computerSelection) => {
   playerSelection = playerSelection.toLowerCase();
@@ -125,50 +122,21 @@ playRound = (playerSelection, computerSelection) => {
   setScore();
 };
 
-// Function to play 5 rounds, add up scores and display the winner at the end.
-// ---- Commenting out for now as this functionality is not needed pending changes to be made adding in a UI ----
-// game = () => {
-//     while (i < 5) {
-//         i++
-//         playRound();
-//     };
-
-//     scores = `Player = ${playerScore}, Computer = ${computerScore}.`
-
-//     if (playerScore == computerScore) {
-//         alert(`GAME OVER. It was a tie, the ending score was ${scores}`)
-//     } else if (playerScore > computerScore) {
-//         alert(`GAME OVER. You are the winner ! The ending score was ${scores}`)
-//     } else if (computerScore > playerScore) {
-//         alert(`GAME OVER. You lost ! The ending score was ${scores}`)
-//     }
-// }
-
 // ----DOM Section----
-
 const rock = document.querySelector(".btn_rock");
 const paper = document.querySelector(".btn_paper");
 const scissors = document.querySelector(".btn_scissors");
 
-// Event listeners to toggle the background color of the buttons and reset them after a certain time aswell as run the playRound function
-
+// Event Listeners
 rock.addEventListener("click", () => {
   choice = rock;
   playRound("Rock");
-  // if (playerSelection != computerSelection) {
-  //     rock.classList.toggle('green');
-  //     setTimeout(() => rock.classList.toggle('green'), 1000)
-  // };
 });
 paper.addEventListener("click", () => {
   choice = paper;
   playRound("Paper");
-  // paper.classList.toggle('green');
-  // setTimeout(() => paper.classList.toggle('green'), 1000)
 });
 scissors.addEventListener("click", () => {
   choice = scissors;
   playRound("Scissors");
-  // scissors.classList.toggle('green');
-  // setTimeout(() => scissors.classList.toggle('green'), 1000)
 });
